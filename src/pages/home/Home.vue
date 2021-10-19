f647cc6e745509c2a65881242232be38f8c6bd97
Tony <gma201011@hotmail.com>
2021-10-19 14:35:27 +0800

    3rd_ver_deploy

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,10 +1,29 @@
 <template>
-    <div>Hello World</div>
+    <div>
+      <home-header></home-header>
+      <home-swiper></home-swiper>
+      <home-icons></home-icons>
+      <home-recommend></home-recommend>
+      <home-weekend></home-weekend>
+    </div>
 </template>
 
 <script>
+import HomeHeader from './components/Header'
+import HomeSwiper from './components/Swiper'
+import HomeIcons from './components/Icons'
+import HomeRecommend from './components/Recommend'
+import HomeWeekend from './components/Weekend.vue'
+
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: {
+    HomeHeader,
+    HomeSwiper,
+    HomeIcons,
+    HomeRecommend,
+    HomeWeekend
+  }
 }
 </script>
 
