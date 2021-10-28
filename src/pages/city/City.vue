03c39c8dcc6524aa6bd54faf90d084b93882f719
Tony <gma201011@hotmail.com>
2021-10-28 15:13:34 +0800

    city_fix

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -26,7 +26,7 @@ export default {
   },
   methods: {
     getCityInfo () {
-      axios.get('/api/city.json')
+      axios.get('/static/mock/api/city.json')
         .then(this.handleGetCityInfoSucc)
     },
     handleGetCityInfoSucc (res) {
