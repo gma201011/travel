649fd04fc4c972cb7db26281e13472dc7cff7be6
Tony <gma201011@hotmail.com>
2021-10-28 14:26:49 +0800

    change

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,15 @@
+# deploy.sh
 #!/usr/bin/env sh
-
-# 当发生错误时中止脚本
+# 當發生錯誤時終止腳本運行
 set -e
-
-# 构建
+# 打包
 npm run build
-
-# cd 到构建输出的目录下 
+# 移動至到打包後的dist目錄 
 cd dist
-
 git init
 git add -A
 git commit -m 'deploy'
-
-# 部署到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:gma201011/Vue-Travel.git main:gh-pages
-
+# 部署到 https://github.com/chou0728/eric-project.git 分支為 gh-pages
+git push -f https://github.com/gma201011/travel.git HEAD:gh-pages
+# 將dist資料夾中的內容推送至遠端eric-project的gh-pages分支中，並強制無條件將舊有的內容取代成目前的內容（指令 git push -f)
 cd -
